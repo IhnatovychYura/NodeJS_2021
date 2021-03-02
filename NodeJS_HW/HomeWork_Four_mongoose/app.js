456c08e6e6bbe808e786095ca36a35e970181de7
IhnatovychYura <yuraihnatovych111@gmail.com>
2021-03-02 21:39:47 +0200

    Node JS Lesson Four

--- NodeJS_HW/HomeWork_Four_mongoose/app.js
+++ NodeJS_HW/HomeWork_Four_mongoose/app.js
@@ -10,10 +10,10 @@ const dirName = __dirname;
 _connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(5000, () => {
-    console.log('App listen 5000')
+    console.log('App listen 5000');
 });
 
 dirService.createDir(dirName);
@@ -21,11 +21,11 @@ dirService.createDir(dirName);
 app.use('/', apiRouter); // or app.all('/', apiRouter);
 
 function _connectDB() {
-    mongoose.connect('mongodb://localhost:27017/node-hw-four', {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect('mongodb://localhost:27017/node-hw-four', { useNewUrlParser: true, useUnifiedTopology: true });
 
     const { connection } = mongoose;
 
     connection.on('error', (error) => {
         console.log(error);
-    })
+    });
 }
