effb3c2c1363235b75db009d3aaa0fb3ffed8385
IhnatovychYura <yuraihnatovych111@gmail.com>
2021-03-19 23:09:25 +0200

    Node JS consultation

--- NodeJS_HW/HomeWork_Seven_nodeMailer/middlewares/auth.middleware.js
+++ NodeJS_HW/HomeWork_Seven_nodeMailer/middlewares/auth.middleware.js
@@ -60,8 +60,6 @@ module.exports = {
                 );
             }
 
-            console.log(access_token);
-
             req.userIdFromAccessToken = tokens._user_id;
 
             next();
@@ -76,7 +74,7 @@ module.exports = {
 
             if (!refresh_token) {
                 throw new ErrorHandler(
-                    statusMessages.TOKEN_REQUIRE[prefLang],
+                    statusMessages.TOKEN_REQUIRE.message[prefLang],
                     statusCode.BAD_REQUEST,
                     statusMessages.TOKEN_REQUIRE.customCode,
                 );
@@ -85,7 +83,7 @@ module.exports = {
             jwt.verify(refresh_token, JWT_REFRESH_SECRET, (err) => {
                 if (err) {
                     throw new ErrorHandler(
-                        statusMessages.NOT_VALID_REFRESH_TOKEN[prefLang],
+                        statusMessages.NOT_VALID_REFRESH_TOKEN.message[prefLang],
                         statusCode.UNAUTHORIZED,
                         statusMessages.NOT_VALID_REFRESH_TOKEN.customCode,
                     );
@@ -98,7 +96,7 @@ module.exports = {
 
             if (!tokens || null) {
                 throw new ErrorHandler(
-                    statusMessages.NOT_VALID_TOKEN_DB[prefLang],
+                    statusMessages.NOT_VALID_TOKEN_DB.message[prefLang],
                     statusCode.NOT_FOUND,
                     statusMessages.NOT_VALID_TOKEN_DB.customCode,
                 );
@@ -117,7 +115,7 @@ module.exports = {
 
             if (userId !== userIdFromAccessToken.id) {
                 throw new ErrorHandler(
-                    statusMessages.UNAUTHORIZED_USER[prefLang],
+                    statusMessages.UNAUTHORIZED_USER.message[prefLang],
                     statusCode.UNAUTHORIZED,
                     statusMessages.UNAUTHORIZED_USER.customCode,
                 );
